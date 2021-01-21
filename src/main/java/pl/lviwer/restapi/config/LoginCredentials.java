01d04765bf150ea043e36ddc42ac785a22837c1a
Lviwer <48951523+Lviwer@users.noreply.github.com>
2021-01-21 12:55:21 +0100

    Login with Http

---
+++ src/main/java/pl/lviwer/restapi/config/LoginCredentials.java
@@ -0,0 +1,12 @@
+package pl.lviwer.restapi.config;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+
+    private String username;
+    private String password;
+
+
+}
---
+++ src/main/java/pl/lviwer/restapi/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package pl.lviwer.restapi.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final DataSource dataSource;
+    private final ObjectMapper objectMapper;
+    private final RestAuthenticationFailureHandler failureHandler;
+    private final RestAuthenticationSuccessHandler successHandler;
+
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .withDefaultSchema() //create default schema for database with users
+                .dataSource(dataSource)
+                .withUser("test")
+                .password("{bcrypt}" + new BCryptPasswordEncoder().encode("test"))
+                .roles("USER");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .addFilter(authenticationFilter() )
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .headers().frameOptions().disable();
+    }
+
+
+
+    public JsonObjectAuthenticationFilter authenticationFilter () throws Exception {
+      JsonObjectAuthenticationFilter authenticationFilter = new JsonObjectAuthenticationFilter(objectMapper);
+      authenticationFilter.setAuthenticationSuccessHandler(successHandler);
+      authenticationFilter.setAuthenticationFailureHandler(failureHandler);
+      authenticationFilter.setAuthenticationManager(super.authenticationManager());
+      return authenticationFilter;
+    }
+
+
+}
